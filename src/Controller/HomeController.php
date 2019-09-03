6ca58f8c1cc4f0aa930a5964ed434c1ec9dff710
Andrew <andri.golovenko@vis-design.com>
2019-09-03 08:27:32 +0300

    Added views

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class HomeController extends AbstractController
 {
@@ -13,8 +16,22 @@ class HomeController extends AbstractController
      */
     public function home()
     {
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
 
-        return $this->render('home.html.twig');
+        return $this->render('home.html.twig', compact('articles'));
+    }
+
+    /**
+     * @Route("/articles/{slug}", methods={"GET"})
+     * @ParamConverter("article", options={"mapping"={"slug"="slug"}})
+     * @param Article $article
+     * @return Response
+     */
+    public function showArticle(Article $article): Response
+    {
+        return $this->render('article.html.twig', compact('article'));
     }
 
 }
